237820ec9ca2a71d9292199a79ff447c6f340159
Agustin <agustin@cooperativamental.com>
2022-11-23 17:09:03 -0300

    ui styles

--- components/Layout/Navbar/Notification/index.jsx
+++ components/Layout/Navbar/Notification/index.jsx
@@ -85,7 +85,7 @@ const Notification = () => {
       height: "75vh",
       justifyContent: "center",
       top: 0,
-      backgroundColor: ["#3BB89F", "#000"],
+      backgroundColor: ["#FA9972", "#000"],
       borderRadius: "0 0 0 1rem",
       transition: {
         duration: 1
@@ -412,7 +412,7 @@ const Notification = () => {
         initial={{ display: "flex", height: "1rem", width: "3rem" }}
         animate={open ? "openContainer" : "closedContainer"}
         ref={refContainer}
-        className={`fixed rounded-l-md flex z-30 right-0 top-3 shadow-sm p-0 bg-[#3BB89F]`}
+        className={`fixed rounded-l-md flex z-30 right-0 top-3 shadow-sm p-0 bg-orange-color`}
       >
         <motion.div
           variants={sidebar}
